4cd2c74f1f659b36206771082b98daf43b0b265b
willymariino <wg.mariino@gmail.com>
2025-02-25 18:39:35 +0100

    console log fizz

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ for (let i = 0; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
     }
-    else
-        console.log(i);
+    else if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+
 }
